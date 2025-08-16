ff605d3f2a4bef041e8d434b0a22cf5411e9a97c
Aditya Chauhan <adityachauhan.ind1@gmail.com>
2025-08-16 16:37:35 +0530

    Footer check updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-100 text-center p-4 mt-10">
-      <p className="text-sm">© 2025 MagicCoupon. All rights reserved.</p>
+      <p className="text-sm">© 2025 MagicCoupon. All rights reserved. aditya chauhan</p>
       
     </footer>
 
